945426a742de55bccb71e750d61b8724fe4c1a17
serhiikurhanskyi <work.kurganskyi@gmail.com>
2021-05-24 23:24:50 +0300

    Create HW_4.py

---
+++ HW_4.py
@@ -0,0 +1,31 @@
+# Введение с клавиатуры
+# my_value = input('Введите любое значение:')
+# print('my_value:', my_value)
+# print('type(my_value):', type(my_value))
+#
+
+# определение типов (int, float, str)
+# my_int = 15
+# print('my_int:', my_int)
+# print('type(my_int):', type(my_int))
+# my_float = 15.4
+# print('my_float:', my_float)
+# print('type(my_float)', type(my_float))
+# my_str = '15'
+# print('my_str:', my_str)
+# print('type(my_str:)', type(my_str))
+
+# Ввод с клавиатуры  дробных значений (функционал int, str)
+# my_int = input('Введите любое значение:')
+# print('my_int:', my_int)
+# print('type(my_int):', type(my_int))
+# my_int = int(my_int)
+# print('type (my_int:', type(my_int))
+
+# Ввод с клавиатуры целочисленых значений (функционал float и  str)
+# my_float = input('Введите любое значение:')
+# print('my_float:', my_float)
+# print('type(my_float:)', type(my_float))
+# my_float = float(my_float)
+# print('my_float:', my_float)
+# print('type(my_float:)', type(my_float))
