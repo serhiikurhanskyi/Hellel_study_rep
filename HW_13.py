550d8dca6fb2c13bc9b166b3b3fe60faad12c139
serhiikurhanskyi <work.kurganskyi@gmail.com>
2021-07-14 23:40:51 +0300

    done HW_13

--- HW_13.py
+++ HW_13.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_dict(d, path):
-     json.dump(d, open(path, 'w'), indent=2)
+    json.dump(d, open(path, 'w'), indent=2)
 
 
 def some_func(*args, **kwargs):
@@ -18,11 +18,10 @@ def some_func(*args, **kwargs):
 def main():
     path = 'res.json'
     tpl = tuple(range(1, 22))
-    # d = dict((tuple([f'bl{l}' * 2] * 2) for l in 'aoeiu'))
+    d = dict((tuple([f'bl{l}' * 2] * 2) for l in 'aoeiu'))
     res = some_func(*tpl, **d)
     print(res)
     load_dict(res, path)
 
-    if __name__ == '__main__':
-        main()
-
+if __name__ == '__main__':
+     main()
